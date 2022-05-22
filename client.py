f77ebdd092591d6e3cc7dcf061724f96ab59bf15
AliMajeed9 <105955807+AliMajeed9@users.noreply.github.com>
2022-05-22 23:41:58 +0300

    Add files via upload

--- client.py
+++ client.py
@@ -1,45 +1,54 @@
 # Connecting to Server
 import socket
-CS = socket.socket()
-host = '127.0.0.1'
-port = 2004
-print('Connecting to Server...')
-try:
-    CS.connect((host, port))
-except socket.error as e:
-    print(str(e))    
 
-print("Connected to Server..")
-res = CS.recv(1024)
-print(res.decode())
-# Identifying yourself and sending username
-username = input('Username: ')
-CS.send(str.encode(username))
-res = CS.recv(1024)
-print(res.decode('utf-8'))
 
-ans = "0"
-while(ans != "5"):
-    # Five Requests
-    print("Do you want to: ")
-    print("1- Arrived Flights Requests.")
-    print("2- Delayed Flights Requests.")
-    print("3- All flights coming from a specific city.")
-    print("4- Details of particular flight.")
-    print("5- Quit..")
-    ans = input()
-    CS.send(str.encode(ans))
-    
-    if (ans == "3"):
-        city = input("Please enter city name: ")
-        CS.send(str.encode(city))
-    
-    if (ans == "4"):
-        iata = input("Please enter flight name: ")
-        CS.send(str.encode(iata))
-        
-        
+class Client:
 
-    res = CS.recv(10000)
-    print(res.decode())
-    print('\n')
+    def __init__(self):
+        self.CS = socket.socket()
+        self.host = '127.0.0.1'
+        self.port = 2004
+        print('Connecting to Server...')
+        try:
+            self.CS.connect((self.host, self.port))
+        except socket.error as e:
+            print(str(e))
+
+        print("Connected to Server..")
+        res = self.CS.recv(1024)
+        print(res.decode())
+        # Identifying yourself and sending username
+        self.username = input('Username: ')
+        self.CS.send(str.encode(self.username))
+        res = self.CS.recv(1024)
+        print(res.decode('utf-8'))
+
+        self.ans = "0"
+        while (self.ans != "5"):
+            # Five Requests
+            print("Do you want to: ")
+            print("1- Arrived Flights Requests.")
+            print("2- Delayed Flights Requests.")
+            print("3- All flights coming from a specific city.")
+            print("4- Details of particular flight.")
+            print("5- Quit..")
+            self.ans = input()
+            self.CS.send(str.encode(self.ans))
+
+            if (self.ans == "3"):
+                city = input("Please enter city name: ")
+                self.CS.send(str.encode(city))
+                print('\n')
+
+            if (self.ans == "4"):
+                iata = input("Please enter flight name: ")
+                self.CS.send(str.encode(iata))
+                print('\n')
+
+            res = self.CS.recv(10000)
+            print(res.decode())
+            print('\n')
+
+
+######################################################3
+client = Client()
